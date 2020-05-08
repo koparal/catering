3c6511c94834d6fdcb0a3bc001e93973fc7190a7
Saban Koparal <sabankoparal@gmail.com>
2020-05-09 02:11:01 +0300

    check trays

--- Catering/Program.cs
+++ Catering/Program.cs
@@ -9,19 +9,14 @@ namespace Catering
 
     class Catering
     {
-
-        public int guests = 2;
-        public int borek = 30;
-        public int cake = 15;
-        public int drink = 30;
-
         const int BOREK = 0;
         const int CAKE = 1;
         const int DRINK = 2;
 
-        int BOREK_LOOP = 2;
-        int CAKE_LOOP = 1;
-        int DRINK_LOOP = 2;
+        // Bug tepsi doldurma kontrolü tepsiX1 olmalı
+        int BOREK_LOOP = 15;
+        int CAKE_LOOP = 0;
+        int DRINK_LOOP = 15;
 
         public int[,] trays = new int[3, 3] {
                                         { 5, 5 ,5 },
@@ -77,22 +72,7 @@ namespace Catering
 
         }
 
-        public void displayRecords()
-        {
-            int rowLength = records.GetLength(0);
-            int colLength = records.GetLength(1);
-
-            for (int i = 0; i < rowLength; i++)
-            {
-                for (int j = 0; j < colLength; j++)
-                {
-                    Console.Write(string.Format("{0} ", records[i, j]));
-                }
-                Console.Write(Environment.NewLine + Environment.NewLine);
-            }
-        }
-
-        public void checkTraysEmpty()
+        public Boolean checkTraysEmpty()
         {
             int rowLength = trays.GetLength(0);
             int colLength = trays.GetLength(1);
@@ -105,34 +85,64 @@ namespace Catering
                     if (trays[i, j] == 0 || trays[i, j] == 1)
                     {
                         int amount = trays[i, j];
-                        Console.WriteLine("Check i {0} j : {1} amount : {2}", i, j, amount);
-                        fillTrays(i, j, amount);
+                        //Console.WriteLine("Check i {0} j : {1} amount : {2}", i, j, amount);
+                        if(fillTrays(i, j, amount) == false )
+                        {
+                            return false;
+                        }
                     }
                 }
             }
+
+            return true;
         }
 
-        public void fillTrays(int tray_no, int food_no, int amount)
+        public Boolean checkTrayByIndex(int tray_no, int food_no,int qty)
         {
-            Console.WriteLine("Fill Trays Tray : {0} Food : {1}, Amount : {2}", tray_no, food_no, amount);
+            int rowLength = trays.GetLength(0);
+            int colLength = trays.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    if(i == tray_no && j == food_no)
+                    {
+                        if(trays[i, j] >= qty)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public Boolean fillTrays(int tray_no, int food_no, int amount)
+        {
+            //Console.WriteLine("Fill Trays Tray : {0} Food : {1}, Amount : {2}", tray_no, food_no, amount);
 
             int selected_food = 0;
 
             if(food_no == BOREK)
             {
-                selected_food = BOREK;
-            }else if(food_no == CAKE)
+                selected_food = BOREK_LOOP;
+            }
+            else if(food_no == CAKE)
             {
-                selected_food = CAKE;
+                selected_food = CAKE_LOOP;
             }
             else
             {
-                selected_food = DRINK;
+                selected_food = DRINK_LOOP;
             }
+
+            
+
             if(selected_food != 0)
             {
-                Console.WriteLine("Tray Update Start");
-                displayTrays();
+                //Console.WriteLine("Tray Update Start");
+                //displayTrays();
                 int result = 0;
                 int oldVal = Convert.ToInt32(trays.GetValue(tray_no, food_no));
 
@@ -152,22 +162,30 @@ namespace Catering
 
                 if (food_no == BOREK)
                 {
-                    BOREK_LOOP--;
+                    BOREK_LOOP-=result;
                 }
                 else if (food_no == CAKE)
                 {
-                     CAKE_LOOP--;
+                     CAKE_LOOP-=result;
                 }
                 else
                 {
-                    DRINK_LOOP--;
+                    DRINK_LOOP-=result;
                 }
 
                 
                 trays.SetValue(result, tray_no, food_no);
                 Console.WriteLine("Tray Updated");
-                displayTrays();
+                //displayTrays();
+
             }
+            else
+            {
+                //Console.WriteLine("Tray Not Updated. Because {0} Tray Count",selected_food);
+                return false;
+            }
+            return true;
+
         }
 
         public void displayTrays()
@@ -180,6 +198,21 @@ namespace Catering
                 for (int j = 0; j < colLength; j++)
                 {
                     Console.Write(string.Format("{0} ", trays[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+        }
+
+        public void displayRecords()
+        {
+            int rowLength = records.GetLength(0);
+            int colLength = records.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", records[i, j]));
                 }
                 Console.Write(Environment.NewLine + Environment.NewLine);
             }
@@ -210,39 +243,51 @@ namespace Catering
 
         static void Main(string[] args)
         {
-
-            /*
-            $arr = [
-                ["5","5","5"],
-                ["5","5","5"],
-                ["5","5","5"],
-            ];
-            */
-
-
             Catering cat = new Catering();
-
+            int guest_id,tray,food,qty;
             
             while (true)
             {
+                cat.displayTrays();
+
+                cat.checkTraysEmpty();
+
                 Console.Write("Which Guest(1-10) : ");
-                int guest_id = Convert.ToInt32(Console.ReadLine());
+                guest_id = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Which Tray (0-2) : ");
-                int tray = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Which Food (borek, cake, drink) : ");
-                string foodName = Console.ReadLine();
-                int food = cat.findFoodByName(foodName);
+                while (true)
+                {
+                    Console.Write("Which Tray (0-2) : ");
+                    tray = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("How Many? :  ");
-                int qty = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Which Food (borek, cake, drink) : ");
+                    string foodName = Console.ReadLine();
+                    food = cat.findFoodByName(foodName);
+
+                    Console.Write("How Many? :  ");
+                    qty = Convert.ToInt32(Console.ReadLine());
+
+                    if (cat.checkTrayByIndex(tray, food, qty))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tepsi de alabileceğiniz kadar miktar yok. Tekrar deneyiniz");
+                        continue;
+                    }
+           
+                }
+            
+                cat.checkTraysEmpty();
 
                 Console.WriteLine("Guest : {0}, Food : {1},Tray : {2}  Qty : {3}", guest_id,food,tray, qty);
 
                 cat.setGuestRecord(guest_id,food, qty);
                 cat.setTrayRecord(tray,food, qty);
 
+                cat.displayTrays();
                 Console.WriteLine("--------------");
             }
             
